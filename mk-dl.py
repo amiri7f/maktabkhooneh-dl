c6ec487d1d72903dc2ed438aa00b61993c3df6c2
what <41821146+AhmadFuzail@users.noreply.github.com>
2020-12-06 22:09:07 +0430

    Update mk-dl.py

--- mk-dl.py
+++ mk-dl.py
@@ -2,7 +2,8 @@ from download import download
 from os import getcwd
 from DownloadLink import DlLink as List
 
-# Athuntecate pewega3654@demail3.com
+#   ToDo !!!! Athuntecate pewega3654@demail3.com
+
 ln = 'https://maktabkhooneh.org/course/%D8%B1%DB%8C%D8%A7%D8%B6%DB%8C-%D8%B9%D9%85%D9%88%D9%85%DB%8C-%DB%B1-mk137/%D9%81%DB%8C%D9%84%D9%85-%D8%A2%D9%85%D9%88%D8%B2%D8%B4%DB%8C-ch185/%D9%88%DB%8C%D8%AF%DB%8C%D9%88-%D8%A2%D9%85%D9%88%D8%B2%D8%B4%DB%8C-%D8%AC%D9%84%D8%B3%D9%87-%D8%AF%D9%88%D9%85-%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF-%D8%B1%DB%8C%D8%A7%D8%B6%DB%8C-%D8%B9%D9%85%D9%88%D9%85%DB%8C-%DB%B1/'
         
 arr = List().DlList(webln = ln)
